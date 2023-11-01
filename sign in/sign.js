31b1b9af5b8dedd40d2ffa26f39644616d7d602e
KRISHNA PRASATH <kprasath@kalvium.community>
2023-11-01 10:46:10 +0530

    done2

--- sign in/sign.js
+++ sign in/sign.js
@@ -10,4 +10,18 @@ var click1 = document.getElementById("submit-button");
 click1.addEventListener("click", function() {  
     window.location.href = '../homepage/homepage.html'; 
 });
+var dropbtn = document.getElementById("myDropdown")
+var selectText = document.getElementById("sel-text")
+
+var li = document.querySelectorAll("#myDropdown>li")
+
+li.forEach((e)=>{
+    e.addEventListener("click",(event)=>{
+        selectText.innerText=event.target.innerText
+        dropdown()
+    })
+})
+function dropdown() {
+    dropbtn.classList.toggle("show")
+}
 
