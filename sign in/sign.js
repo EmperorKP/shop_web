978d4da1ac7e0a850a9af0e2a02a2abca9c1f771
KRISHNA PRASATH <kprasath@kalvium.community>
2023-11-01 09:44:27 +0530

    done

---
+++ sign in/sign.js
@@ -0,0 +1,13 @@
+var click = document.getElementById("button");
+
+
+click.addEventListener("click", function() {  
+    window.location.href = 'https://www.shop.com/nbts/create-myaccount.xhtml?ischeckout=false'; 
+});
+
+var click1 = document.getElementById("submit-button");
+
+click1.addEventListener("click", function() {  
+    window.location.href = '../homepage/homepage.html'; 
+});
+
